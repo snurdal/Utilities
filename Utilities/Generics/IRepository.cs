7b1eb2f2ef2627db778e79ad041b5a9cbee22702
Seyma Nur Dalamanli <seymanurdalamanli@gmail.com>
2025-06-29 00:01:27 +0300

    I coded Generics<IRepository>

---
+++ Utilities/Generics/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Generics
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task InsertOneAsync(TEntity entity);
+        Task InsertManyAsync(IEnumerable<TEntity> entities);
+
+        Task<TEntity?> FindByKeyAsync(object entityKey);
+        Task<TEntity?> FindFirstAsync(Expression<Func<TEntity, bool>>? expression = null, params string[] includes);
+        Task<IEnumerable<TEntity>> FindManyAsync(Expression<Func<TEntity, bool>>? expression = null, params string[] includes);
+
+        Task UpdateOneAsync(TEntity entity);
+        Task UpdateManyAsync(IEnumerable<TEntity> entities);
+
+        Task DeleteByIdAsync(object entityKey);
+        Task DeleteAsync(TEntity entity);
+        Task DeleteManyAsync(Expression<Func<TEntity, bool>>? expression = null);
+
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>>? expression = null);
+        Task<int> CountAsync(Expression<Func<TEntity, bool>>? expression = null);
+    }
+}
